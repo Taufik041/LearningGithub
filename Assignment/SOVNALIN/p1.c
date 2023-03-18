3266abc3373c68a229ac05f6fe27a96881eca2f1
Taufik Khan <99543191+Taufik041@users.noreply.github.com>
2023-03-18 21:39:54 +0530

    Update p1.c

--- Assignment/SOVNALIN/p1.c
+++ Assignment/SOVNALIN/p1.c
@@ -5,7 +5,7 @@ void main()
     for(i=1;i<=100;i++)
     {
         if(i%3==0&&i%5==0)
-            printf("%d\n",i);
+            printf("Fizz Buzz\n");
         else{
             if(i%3==0)
             printf("Fizz\n");
