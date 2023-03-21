c8ff7d449a02a9b1d497305104370a627e29d323
Swapnil <swapnilnanda312@gmail.com>
2023-03-21 19:33:58 +0530

    rectified

--- Assignment/Swapnil/Week_2_assignment_questions/p1.c
+++ Assignment/Swapnil/Week_2_assignment_questions/p1.c
@@ -1,21 +1,22 @@
 // Write a program to count the number of occurences of a number in an array using function.
 
 # include <stdio.h>
-
-int main()
+void arr_input( int*, int);
+int cnt_no_of_occurences(int*,int,int);
+void arr_input(int *a, int n)
 {
-    int len,n,count = 0;
-    printf("Enter no of elements in an array : ");
-    scanf("%d",&len);
-    int a[len];
-    for(int i = 0; i < len; i++)
+    for(int i = 0; i < n; i++)
     {
         printf("Enter element %d : ",(i+1));
-        scanf("%d", &a[i]);
+        scanf("%d",a);
+        a++;
     }
-    printf("Enter the element you want to find occurence of : ");
-    scanf("%d", &n);
-    for(int j = 0; j < len; j++)
+
+}
+int cnt_no_of_occurences(int *a, int l, int n)
+{
+    int count = 0;
+    for(int j = 0; j < l; j++)
     {
         if(n == a[j])
         {
@@ -23,5 +24,18 @@ int main()
         }
 
     }
-    printf("%d is present %d times.",n,count);
+    return count;
+}
+int main()
+{
+    int len,n,count = 0;
+    printf("Enter no of elements in an array : ");
+    scanf("%d",&len);
+    int a[len];
+    int *p = &a[0];
+    arr_input(p,len);
+    printf("Enter the element you want to find occurence of : ");
+    scanf("%d", &n);
+    
+    printf("%d is present %d times.",n,cnt_no_of_occurences(p,len,n));
 }
