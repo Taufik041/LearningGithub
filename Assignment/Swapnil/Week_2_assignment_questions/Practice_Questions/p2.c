1bf083d5b7cd50e0e9dd3e9c9f642ed604cce784
Swapnil <swapnilnanda312@gmail.com>
2023-03-24 01:03:21 +0530

    rectified

--- Assignment/Swapnil/Week_2_assignment_questions/Practice_Questions/p2.c
+++ Assignment/Swapnil/Week_2_assignment_questions/Practice_Questions/p2.c
@@ -1,1 +1,51 @@
-Write  a program to convert a decimal n
+// Write  a program to convert a decimal number to a binary number using functions.
+# include <stdio.h>
+# include <string.h>
+
+int main()
+{
+    
+    int a;
+    printf("Enter a positive number : ");
+    scanf("%d",&a);
+    if(a < 0)
+    {
+        a = -a;
+    }
+    else
+    {
+        a = a;
+    }
+    int n = a;
+    char b[32];
+    int j = 0;
+    int zero = 48;
+    int one = 49;
+    int r;
+    int c = 0;
+    while(a > 0)
+    {
+        c++;
+        r = a%2;
+        if(r == 0)
+        {
+            b[j] = (int)zero;
+        }
+        else if(r == 1)
+        {
+            b[j] = (int)one;
+        }
+        
+        a = a/2;
+        j++;
+        
+    }
+    printf("Binary equivalent of %d = ",n);
+    for(int i=c-1; i >= 0; i--)
+    {
+        printf("%c",b[i]);
+    }
+    
+    
+    return 0;
+}
